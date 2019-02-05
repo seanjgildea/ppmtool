34b0f3fede0298e746c7dec80c219a9c0754161e
Sean J Gildea <seanjgildea@gmail.com>
2019-02-05 11:13:51 -0500

    Custom exception for project task. closes #15

--- src/main/java/io/agileintelligence/ppmtool/exception/ProjectNotFoundException.java
+++ src/main/java/io/agileintelligence/ppmtool/exception/ProjectNotFoundException.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@Data
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ProjectNotFoundException extends RuntimeException {
 
--- src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -1,9 +1,11 @@
 package io.agileintelligence.ppmtool.services;
 
 import io.agileintelligence.ppmtool.domain.Backlog;
+import io.agileintelligence.ppmtool.domain.Project;
 import io.agileintelligence.ppmtool.domain.ProjectTask;
 import io.agileintelligence.ppmtool.exception.ProjectNotFoundException;
 import io.agileintelligence.ppmtool.repositories.BacklogRespository;
+import io.agileintelligence.ppmtool.repositories.ProjectRepository;
 import io.agileintelligence.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,9 @@ public class ProjectTaskService {
 
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
+
+    @Autowired
+    private ProjectRepository projectRepository;
 
     public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
 
@@ -56,6 +61,37 @@ public class ProjectTaskService {
     }
 
     public Iterable<ProjectTask>findBacklogById(String id) {
+
+        Project project = projectRepository.findByProjectIdentifier(id);
+
+        if ( project == null ) {
+            throw new ProjectNotFoundException("Project with ID: '" + id + "' does not exist");
+        }
+
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
+    }
+
+    public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id) {
+
+        // make sure we are searching on an existing backlog
+        Backlog backlog = backlogRespository.findByProjectIdentifier(backlog_id);
+
+        if (backlog == null ) {
+            throw new ProjectNotFoundException("Project with ID '" + backlog_id + "' not found");
+        }
+
+        // make sure that our task exists
+        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+
+        if ( projectTask == null ) {
+            throw new ProjectNotFoundException("Project Task '" + pt_id + "' not found");
+        }
+        // make sure that the backlog/project id in the path corresponds to the right project
+
+        if ( !projectTask.getProjectIdentifier().equals(backlog_id)) {
+            throw new ProjectNotFoundException("Project Task '" + pt_id + "'does not exist in project '" + backlog_id);
+        }
+
+        return projectTask;
     }
 }
