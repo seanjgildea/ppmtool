5a4a82ce1e8c93a9ffa64d5380b8c41fabd7571e
Sean J Gildea <seanjgildea@gmail.com>
2019-02-07 16:41:38 -0500

    User repo entity service closes #18

--- src/main/java/io/agileintelligence/ppmtool/domain/User.java
+++ src/main/java/io/agileintelligence/ppmtool/domain/User.java
@@ -1,4 +1,34 @@
 package io.agileintelligence.ppmtool.domain;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email(message = "Username needs to be an email")
+    @NotBlank( message = "Username is required")
+    @Column( unique = true )
+    private String username;
+
+    @NotBlank(message = "Please enter your full name")
+    private String fullname;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @Transient
+    private String confirmPassword;
+
+    private Date created_At;
+    private Date updated_At;
 }
