0673dd17574b8c4eecf09cab53376681de59a872
Sean J Gildea <seanjgildea@gmail.com>
2019-02-08 08:14:11 -0500

    Setup security and custom User. Closes #17

--- src/main/java/io/agileintelligence/ppmtool/domain/User.java
+++ src/main/java/io/agileintelligence/ppmtool/domain/User.java
@@ -1,15 +1,19 @@
 package io.agileintelligence.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import java.util.Collection;
 import java.util.Date;
 
 @Entity
 @Data
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,4 +35,38 @@ public class User {
 
     private Date created_At;
     private Date updated_At;
+
+    @PrePersist
+    protected void onCreate() { this.created_At = new Date(); }
+
+    @PreUpdate void onUpdate() { this.updated_At = new Date(); }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isEnabled() {
+        return true;
+    }
 }
